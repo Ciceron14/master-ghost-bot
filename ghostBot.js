6c57510e9b2d8f2d75fa5029ab2d4d54c72d2e7a
Mike <mikegeorges.pro@gmail.com>
2017-07-12 21:18:43 -0400

    hum

--- ghostBot.js
+++ ghostBot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 var fs = require('fs');
 //var finder = require('findit').find(__dirname);
 
-let planned_ops = new Discord.Message();
+let planned_ops = new Discord.Message().get(334856205174439937);
 
 function arguments(str)
 {
