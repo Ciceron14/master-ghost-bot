a07e2c61814872d4ced6670c62a904ff35c4b6ca
Mike <mikegeorges.pro@gmail.com>
2017-07-18 20:51:35 -0400

    made a typo mb

--- ghostBot.js
+++ ghostBot.js
@@ -110,7 +110,7 @@ client.on('message', message =>
                     for (var [ID, plan] of Array.from(OG)) 
                     {
                         plan.edit("Edited that fucker");
-                        contsole.log(plan.toString);
+                        console.log(plan.toString);
                     }
                     message.channel.sendMessage('The fireteam "' + fireteamName + '" has been created !');
                     
