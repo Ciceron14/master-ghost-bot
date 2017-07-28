fab81d27c77ccf655dd8e52c832f4bb74abf3699
Mike <mikegeorges.pro@gmail.com>
2017-07-28 19:50:02 -0400

    Cleaned up some code Added variables Bot commands not all available in all channels New way to create and store fireteams (not ready yet)

--- ghostBot.js
+++ ghostBot.js
@@ -4,7 +4,50 @@ const client = new Discord.Client();
 var fs = require('fs');
 
 var initializing = [];
-var guildID = "292076742355451904"
+
+//Guild info
+var guildID = "292076742355451904";
+var ghostID = "301176884438368257";
+
+//Channels
+var open_broadcastID = "292076742355451904";
+var planned_operationsID = "315332691576750080";
+var tactical_roundtableID = "315704892906012674";
+var public_frequencyID = "296718956545441794";
+var space_nonsenseID = "315561936668590081";
+var test_channelID ="301180453883478016";
+
+//Roles
+var meme_available = "339897537584693258";
+
+
+
+//FUNCTIONS
+function realName(str)
+{
+    var pattern = " [";
+    if (str && str.length)
+    {
+        trumped = str;
+        if (pattern && pattern.length)
+        {
+            var idx = str.indexOf(pattern);
+
+            if (idx != -1)
+            {
+                trumped = str.substring(0, idx);
+            }
+        }
+    }
+    return (trumped);
+}
+
+function psnID(str)
+{
+    var start_pos = str.indexOf('[') + 1;
+    var end_pos = str.indexOf(']', start_pos);
+    return str.substring(start_pos, end_pos);
+}
 
 function arguments(str)
 {
@@ -13,10 +56,10 @@ function arguments(str)
     return str.substring(start_pos, end_pos);
 }
 
-function plannedOperations(ID, str)
+function findFireteam(ID, str)
 {
-    client.channels.get('315332691576750080').fetchMessage(ID)
-        .then(message => message.edit(message.content + str))
+    client.channels.get('315332691576750080').fetchPinnedMessages()
+        .then(messages => console.log(`Received ${messages.size} messages`))//message.edit(message.content + str))
         .catch(console.error);
 }
 
@@ -25,12 +68,20 @@ client.on('ready', () =>
     console.log('Ghost is online');
 });
 
+
+
+
+
+
+
+
+
 //WELCOME AND GOODBYE
 client.on('guildMemberAdd', member =>
 {
     let guild = member.guild;
-    guild.channels.get('296718956545441794').send("I'm receiving signals... I think it's a guardian !");
-    guild.channels.get('296718956545441794').send("I'll try to initialize them");
+    guild.channels.get(public_frequencyID).send("I'm receiving signals... I think it's a guardian !");
+    guild.channels.get(public_frequencyID).send("I'll try to initialize them");
 
     initializing.push([member.id, 1]);
     member.sendMessage("Guardian ? This is the Conglomerate's frequency. Your signal is very weak...");
@@ -40,29 +91,76 @@ client.on('guildMemberAdd', member =>
 client.on('guildMemberRemove', member =>
 {
     let guild = member.guild;
-    guild.channels.get('296718956545441794').send('Guardian down ! ' + member.user + ' left the Conglomerate.\nOnly ' + (guild.memberCount - 1) + " guardians left.");
+    guild.channels.get(public_frequencyID).send('Guardian down ! ' + member.user + ' left the Conglomerate.\nOnly ' + (guild.memberCount - 1) + " guardians left.");
 });
+
+
+
+
+
+
 
 
 //REACT TO MESSAGES IN CHANNEL
 client.on('message', message =>
 {
-    if (message.channel.id == "315561936668590081")
+    //MEMES AND ALL
+    if (message.channel.id == space_nonsenseID)
     {
-        client.guilds.get("292076742355451904").members.get(message.author.id).hoistRole
+        client.guilds.get(guildID).members.get(message.author.id).hoistRole
         //var rolesList = [].concat.apply([], client.guilds.get("292076742355451904").members.get(message.author.id).roles)
-        if (client.guilds.get("292076742355451904").members.get(message.author.id).roles.has("339897537584693258"))
+        if (client.guilds.get(guildID).members.get(message.author.id).roles.has(meme_available))
         {
-            client.guilds.get("292076742355451904").members.get(message.author.id).removeRole("339897537584693258");
+            client.guilds.get(guildID).members.get(message.author.id).removeRole(meme_available);
         }
         else
         {
             message.delete();
             message.author.sendMessage("Wait until you have a new meme available, check your roles");
         }
+
+        //Bot tells a destiny pun
+        if (message.content.toLowerCase().toString().includes('pun') || message.content.toLowerCase().toString().includes('joke'))
+        {
+            jokes = [
+                "I had a very good joke, but I Phogoth it.",
+                "Why are titans' eyes always hurting ? \nBecause they can't blink.",
+                "Why shouldn't you have sex with the Templar? \nBecause he has harpies.",
+                "Why did the Archon Priest get locked up? \nVandallism.",
+                "What do you call two acolytes claping hands? \nA Hive five.",
+                "Why did the Archon Priest get locked up? \nVandallism.",
+                "Hey, are your pants purple ? \nBecause that ass is legendary.",
+                "I have a crush on one of the Archons... I guess you could say I've Fallen in love.",
+                "I went to a Hive party last week. \nIt was pretty enthralling.",
+                "Why can't Xur get a girlfriend ? \nBecause he doesn't have any Icebreaker.",
+                "What is a Knight's favorite game ? \nHive and seek.",
+                "What do Voidwalkers apply to chaffed skin ? \nNova Balm.",
+                "Why are some servitors called 'High Servitors' ? \nDregs.",
+                "Okay, seriously, one more bad pun and I'm getting the Hellmouth of here.",
+                "What do bowling pins hate in Destiny ? \nStrikes.",
+                "I wanted to make a joke... but all the good ones Archon.",
+                "I never understood the hype about the Kellslayer's Helm. \nIt looks rather plane to me.",
+                "Yo mamma so fat that when she puts on Chatterwhite everybody thinks she's the Traveller",
+                "What do Hive give each others on Christmas ? \nPresents of Crota.",
+                "When I die I want my LFG team to burry me. \nSo they can let me down one last time.",
+                "What does the Traveller blame when his message doesn't get accros ? \nHis Speakers.",
+                "What do the Vex eat for breakfast ? \nCornfluxes.",
+                "How does Oryx measure his ships' speed ? \nIn Dreadknots"
+                ];
+            i = Math.floor(Math.random() * (jokes.length));
+            message.channel.send(jokes[i]);
+        }
+
     }
-    //TESTING INIT
-    if (message.channel.type == "dm" && message.author.id != "301176884438368257")
+
+
+
+
+
+
+
+    //DIRECT & INIT
+    if (message.channel.type == "dm" && message.author.id != ghostID)
     {
         var flat = [].concat.apply([], initializing);
         var col = flat.indexOf(message.author.id);
@@ -146,56 +244,71 @@ client.on('message', message =>
         }
     }
 
-    if (message.isMentioned("301176884438368257"))
+
+
+
+
+
+
+    if (message.isMentioned(ghostID))
     {
-        //TESTING INIT
-        if (message.content.toLowerCase().toString().includes('initialize'))
-        {
-            initializing.push([message.author.id, 1]);
-            message.author.sendMessage("Guardian ? This is the Conglomerate's frequency. Your signal is very weak...");
-            message.author.sendMessage("Guardian, do you copy ? Type `yes` if you can hear me. Type `no` if you can't... if that makes sense ?");
-        }
 
         //Bot informs member of its status
-        else if (message.content.toLowerCase().toString().includes('status'))
+        if (message.content.toLowerCase().toString().includes('status'))
         {
-            message.channel.send('I am learning how to initialize new members, ' + trump(message.member.displayName, " [") + "\nI am also working on a way to stock fireteams, to make it easier for you to plan upcoming games.");
+            message.channel.send('I can now initialize new members, ' + realName(message.member.displayName) + "\nI am also working on a way to stock fireteams, to make it easier for you to plan upcoming games.");
         }
 
-        //Bot does research in ishtar database
-        else if (message.content.toLowerCase().toString().includes('search') && message.content.toLowerCase().toString().includes('ishtar'))
+
+
+
+
+
+
+        //TACTICS AND KNOWLEDGE
+        if(message.channel.id == tactical_roundtableID)
         {
-            argString = arguments(message.content.toLowerCase().toString());
-            if (argString.length === 0)
+            //Bot does research in ishtar database
+            if (message.content.toLowerCase().toString().includes('search') && message.content.toLowerCase().toString().includes('ishtar'))
             {
-                message.channel.send('I need some keywords to find lore entries Guardian. Please include `"keywords"` in your request.');
-            }
-            else
-            {
-                args = argString.split(" ");
-                var keywords = args.length + 1;
-                message.channel.send("I was given " + (keywords - 1) + " keywords. Here is what I found :");
-                var search = '';
-                while (keywords > 1)
+                argString = arguments(message.content.toLowerCase().toString());
+                if (argString.length === 0)
                 {
-                    search += args[args.length - keywords + 1] + "-";
-                    keywords -= 1;
+                    message.channel.send('I need some keywords to find lore entries Guardian. Please include `"keywords"` in your request.');
                 }
-                message.channel.send('http://www.ishtar-collective.net/categories/' + search.substring(0, search.length - 1));
+                else
+                {
+                    args = argString.split(" ");
+                    var keywords = args.length + 1;
+                    message.channel.send("I was given " + (keywords - 1) + " keywords. Here is what I found :");
+                    var search = '';
+                    while (keywords > 1)
+                    {
+                        search += args[args.length - keywords + 1] + "-";
+                        keywords -= 1;
+                    }
+                    message.channel.send('http://www.ishtar-collective.net/categories/' + search.substring(0, search.length - 1));
+                }
             }
         }
+        
+        
 
-        //Bot manages fireteams
-        else if (message.content.toLowerCase().toString().includes('fireteam'))
+
+
+
+
+
+        //LFG AND OPERATIONS
+        if(message.channel.id == planned_operationsID)
         {
-            //planned_ops = message.channel.lastMessageID;
             //CREATE
-            if (message.content.toLowerCase().toString().includes('new'))
+            if (message.content.toLowerCase().toString().includes('new') || message.content.toLowerCase().toString().includes('create'))
             {
                 argString = arguments(message.content.toLowerCase().toString());
                 if (argString.length <= 0)
                 {
-                    message.channel.send('I need a fireteam name Guardian. Please include `new, fireteam, "fireteam name"` in your request.');
+                    message.channel.send('I need a fireteam name Guardian. Please include `new *or* create *+* "fireteam name"` in your request.');
                 }
                 else
                 {
@@ -208,17 +321,9 @@ client.on('message', message =>
                         keywords -= 1;
                     }
                     fireteamName.substring(0, fireteamName.length - 1);
-                    //TEST
-                    /*if (planned_ops == null)
-                    {
-                        planned_ops = message.member.guild.channels.get('315332691576750080').send(fireteamName + ".txt" + ' \n' + message.member.displayName.toString() + ' \n').id;
-                    }
-                    else
-                    {
-                        message.planned_ops.edit("Is it working ?");
-                    }*/
-                    plannedOperations('334875452554608640', "Edited that fucker");
-                    message.channel.send('The fireteam "' + fireteamName + '" has been created !');
+
+                    //findFireteam('334875452554608640', "Edited that fucker");
+                    message.channel.send('Fireteam: ' + fireteamName + '\n- ' + psnID(message.member.displayName));
                     
                     /*fs.writeFile("fireteams/-" + fireteamName + ".txt", message.member.displayName.toString() + ' \n', function (err)
                     {
@@ -228,7 +333,7 @@ client.on('message', message =>
                         }
                         else
                         {
-                            message.channel.send('Your fireteam "' + fireteamName + '" has been created !');
+                                message.channel.send('Your fireteam "' + fireteamName + '" has been created !');
                         }
                     });*/
                 }
@@ -238,7 +343,7 @@ client.on('message', message =>
                 argString = arguments(message.content.toLowerCase().toString());
                 if (argString.length <= 0)
                 {
-                    message.channel.send('You need a fireteam to join a fireteam. Please include `join, fireteam, "fireteam name"` in your request.');
+                    message.channel.send('You need a fireteam to join a fireteam. Please include `join *+* "fireteam name"` in your request.');
                 }
                 else
                 {
@@ -262,9 +367,9 @@ client.on('message', message =>
                             {
                                 message.channel.send('Looks like you already joined that fireteam, here is the current roster :')
                                 fs.readFile("fireteams/-" + fireteamName + ".txt", 'utf8', function (err, data)
-                                {
+                                 {
                                     if (err)
-                                    {
+                                     {
                                         message.channel.send('Hum... well I think you joined but I cannot find it... weird.')
                                     }
                                     message.channel.send(data);
@@ -294,7 +399,8 @@ client.on('message', message =>
             else if (message.content.toLowerCase().toString().includes('delete'))
             {
                 argString = arguments(message.content.toLowerCase().toString());
-                if (argString.length <= 0) {
+                if (argString.length <= 0) 
+                {
                     message.channel.send('I need a fireteam name to be able to delete it. Please include `delete, fireteam, "fireteam name"` in your request.');
                 }
                 else
@@ -325,64 +431,26 @@ client.on('message', message =>
                 message.channel.send('Did you say something guardian ? Add `new`, `join`, `delete` or `show` to your fireteams request.')
             }
         }
+        
 
-        //Bot tells a destiny pun
-        else if (message.content.toLowerCase().toString().includes('pun') || message.content.toLowerCase().toString().includes('joke'))
-        {
-            jokes = [
-                "I had a very good joke, but I Phogoth it.",
-                "Why are titans' eyes always hurting ? \nBecause they can't blink.",
-                "Why shouldn't you have sex with the Templar? \nBecause he has harpies.",
-                "Why did the Archon Priest get locked up? \nVandallism.",
-                "What do you call two acolytes claping hands? \nA Hive five.",
-                "Why did the Archon Priest get locked up? \nVandallism.",
-                "Hey, are your pants purple ? \nBecause that ass is legendary.",
-                "I have a crush on one of the Archons... I guess you could say I've Fallen in love.",
-                "I went to a Hive party last week. \nIt was pretty enthralling.",
-                "Why can't Xur get a girlfriend ? \nBecause he doesn't have any Icebreaker.",
-                "What is a Knight's favorite game ? \nHive and seek.",
-                "What do Voidwalkers apply to chaffed skin ? \nNova Balm.",
-                "Why are some servitors called 'High Servitors' ? \nDregs.",
-                "Okay, seriously, one more bad pun and I'm getting the Hellmouth of here.",
-                "What do bowling pins hate in Destiny ? \nStrikes.",
-                "I wanted to make a joke... but all the good ones Archon.",
-                "I never understood the hype about the Kellslayer's Helm. \nIt looks rather plane to me.",
-                "Yo mamma so fat that when she puts on Chatterwhite everybody thinks she's the Traveller",
-                "What do Hive give each others on Christmas ? \nPresents of Crota.",
-                "When I die I want my LFG team to burry me. \nSo they can let me down one last time.",
-                "What does the Traveller blame when his message doesn't get accros ? \nHis Speakers.",
-                "What do the Vex eat for breakfast ? \nCornfluxes.",
-                "How does Oryx measure his ships' speed ? \nIn Dreadknots"
-                ];
-            i = Math.floor(Math.random() * (jokes.length));
-            message.channel.send(jokes[i]);
-        }
+
+
+
+
 
         //Bot is mentionned but no command is given
         else
         {
-            message.channel.send('How can I help you, ' + trump(message.member.displayName, " ["));
+            message.channel.send('How can I help you, ' + realName(message.member.displayName));
         }
     }
 });
 
-function trump(str, pattern)
-{
-    if (str && str.length)
-    {
-        trumped = str;
-        if (pattern && pattern.length)
-        {
-            var idx = str.indexOf(pattern);
 
-            if (idx != -1)
-            {
-                trumped = str.substring(0, idx);
-            }
-        }
-    }
-    return (trumped);
-}
+
+
+
+
 
 //add bot to server = https://discordapp.com/oauth2/authorize?client_id=301176884438368257&scope=bot
 //bot client id = 301176884438368257
